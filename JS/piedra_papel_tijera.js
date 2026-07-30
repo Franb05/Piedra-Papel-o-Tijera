5fdbc6af84207957557f6b03116639f623d6f499
Franb05 <146026328+Franb05@users.noreply.github.com>
2026-07-30 16:17:23 -0300

    Implement human and computer choice functions

--- JS/piedra_papel_tijera.js
+++ JS/piedra_papel_tijera.js
@@ -1,1 +1,19 @@
-console.log("Hola mundo");
+function getHumanChoice() {
+  const usuario = prompt("Ingrese piedra, papel o tijera");
+  return usuario;
+}
+
+console.log(getHumanChoice());
+
+function getComputerChoice() {
+  const computadora = Math.floor(Math.random() * 3);
+  if (computadora === 0) {
+    return "Piedra";
+  } else if (computadora === 1) {
+    return "Papel";
+  } else {
+    return "Tijera";
+  }
+}
+
+console.log(getComputerChoice());
