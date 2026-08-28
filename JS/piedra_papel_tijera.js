c29c3de8c05634cf6cb4053c4d22e9705190afdd
Franb05 <146026328+Franb05@users.noreply.github.com>
2026-08-28 01:10:44 -0300

    Add click event listeners to buttons

--- JS/piedra_papel_tijera.js
+++ JS/piedra_papel_tijera.js
@@ -1,56 +1,68 @@
-//Variables Globales
-let humanScore = 0;
-let computerScore = 0;
+document.addEventListener("DOMContentLoaded", () => {
+  // Seleccionamos los botones y agregamos el evento click
+  const piedraBtn = document.getElementById("btn-piedra");
+  const papelBtn = document.getElementById("btn-papel");
+  const tijeraBtn = document.getElementById("btn-tijera");
 
-//Funciones
-function getHumanChoice() {
-  const usuario = prompt("Ingrese piedra, papel o tijera").trim().toLowerCase();
-  /*
+  piedraBtn.addEventListener("click", () => {});
+  papelBtn.addEventListener("click", () => {});
+  tijeraBtn.addEventListener("click", () => {});
+
+  //Variables Globales
+  let humanScore = 0;
+  let computerScore = 0;
+
+  //Funciones
+  function getHumanChoice() {
+    const usuario = prompt("Ingrese piedra, papel o tijera")
+      .trim()
+      .toLowerCase();
+    /*
   .trim : Elimina los espacios en blanco al principio y al final de un texto
   .toLowerCase: Sirve para convertir siempre la entrada del usuario a minúsculas
   */
-  return usuario;
-}
-
-//console.log(getHumanChoice());
-
-function getComputerChoice() {
-  const computadora = Math.floor(Math.random() * 3);
-  if (computadora === 0) {
-    return "piedra";
-  } else if (computadora === 1) {
-    return "papel";
-  } else {
-    return "tijera";
+    return usuario;
   }
-}
 
-//console.log(getComputerChoice());
+  //console.log(getHumanChoice());
 
-function playRound(humanChoice, computerChoice) {
-  if (humanChoice === computerChoice) {
-    return "EMPATE";
-  } else if (humanChoice === "piedra" && computerChoice === "papel") {
-    computerScore++;
-    return "PERDISTE";
-  } else if (humanChoice === "piedra" && computerChoice === "tijera") {
-    humanScore++;
-    return "GANASTE";
-  } else if (humanChoice === "papel" && computerChoice === "piedra") {
-    humanScore++;
-    return "GANASTE";
-  } else if (humanChoice === "papel" && computerChoice === "tijera") {
-    computerScore++;
-    return "PERDISTE";
-  } else if (humanChoice === "tijera" && computerChoice === "piedra") {
-    computerScore++;
-    return "PERDISTE";
-  } else if (humanChoice === "tijera" && computerChoice === "papel") {
-    humanScore++;
-    return "GANASTE";
+  function getComputerChoice() {
+    const computadora = Math.floor(Math.random() * 3);
+    if (computadora === 0) {
+      return "piedra";
+    } else if (computadora === 1) {
+      return "papel";
+    } else {
+      return "tijera";
+    }
   }
-}
-/* PRUEBA DE PLAY ROUND
+
+  //console.log(getComputerChoice());
+
+  function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+      return "EMPATE";
+    } else if (humanChoice === "piedra" && computerChoice === "papel") {
+      computerScore++;
+      return "PERDISTE";
+    } else if (humanChoice === "piedra" && computerChoice === "tijera") {
+      humanScore++;
+      return "GANASTE";
+    } else if (humanChoice === "papel" && computerChoice === "piedra") {
+      humanScore++;
+      return "GANASTE";
+    } else if (humanChoice === "papel" && computerChoice === "tijera") {
+      computerScore++;
+      return "PERDISTE";
+    } else if (humanChoice === "tijera" && computerChoice === "piedra") {
+      computerScore++;
+      return "PERDISTE";
+    } else if (humanChoice === "tijera" && computerChoice === "papel") {
+      humanScore++;
+      return "GANASTE";
+    }
+  }
+  /* PRUEBA DE PLAY ROUND
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
@@ -59,23 +71,24 @@ console.log(humanScore);
 console.log(computerScore);
 */
 
-function playGame() {
-  {
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
+  function playGame() {
+    {
+      const humanSelection = getHumanChoice();
+      const computerSelection = getComputerChoice();
 
-    console.log(playRound(humanSelection, computerSelection));
-    console.log("Usuario= ", humanScore);
-    console.log("Computadora= ", computerScore);
+      console.log(playRound(humanSelection, computerSelection));
+      console.log("Usuario= ", humanScore);
+      console.log("Computadora= ", computerScore);
+    }
+
+    if (humanScore > computerScore) {
+      console.log("🎉 ¡Felicitaciones! Ganaste la partida.");
+    } else if (computerScore > humanScore) {
+      console.log("💻 La computadora ganó la partida. ¡Intentá nuevamente!");
+    } else {
+      console.log("🤝 La partida terminó en empate.");
+    }
   }
 
-  if (humanScore > computerScore) {
-    console.log("🎉 ¡Felicitaciones! Ganaste la partida.");
-  } else if (computerScore > humanScore) {
-    console.log("💻 La computadora ganó la partida. ¡Intentá nuevamente!");
-  } else {
-    console.log("🤝 La partida terminó en empate.");
-  }
-}
-
-playGame();
+  playGame();
+});
