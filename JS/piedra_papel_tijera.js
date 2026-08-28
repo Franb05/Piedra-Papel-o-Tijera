df634cf02763a6f96d1b103065aea24cf7441a8f
Franb05 <146026328+Franb05@users.noreply.github.com>
2026-08-28 01:31:49 -0300

    Remove old game input logic

--- JS/piedra_papel_tijera.js
+++ JS/piedra_papel_tijera.js
@@ -4,27 +4,24 @@ document.addEventListener("DOMContentLoaded", () => {
   const papelBtn = document.getElementById("btn-papel");
   const tijeraBtn = document.getElementById("btn-tijera");
 
-  piedraBtn.addEventListener("click", () => {});
-  papelBtn.addEventListener("click", () => {});
-  tijeraBtn.addEventListener("click", () => {});
+  piedraBtn.addEventListener("click", () => {
+    playRound("piedra", getComputerChoice());
+    console.log("Hiciste click en Piedra");
+  });
+  papelBtn.addEventListener("click", () => {
+    playRound("papel", getComputerChoice());
+    console.log("Hiciste click en Papel");
+  });
+  tijeraBtn.addEventListener("click", () => {
+    playRound("tijera", getComputerChoice());
+    console.log("Hiciste click en Tijera");
+  });
 
   //Variables Globales
   let humanScore = 0;
   let computerScore = 0;
 
   //Funciones
-  function getHumanChoice() {
-    const usuario = prompt("Ingrese piedra, papel o tijera")
-      .trim()
-      .toLowerCase();
-    /*
-  .trim : Elimina los espacios en blanco al principio y al final de un texto
-  .toLowerCase: Sirve para convertir siempre la entrada del usuario a minúsculas
-  */
-    return usuario;
-  }
-
-  //console.log(getHumanChoice());
 
   function getComputerChoice() {
     const computadora = Math.floor(Math.random() * 3);
@@ -62,33 +59,4 @@ document.addEventListener("DOMContentLoaded", () => {
       return "GANASTE";
     }
   }
-  /* PRUEBA DE PLAY ROUND
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-console.log(playRound(humanSelection, computerSelection));
-console.log(humanScore);
-console.log(computerScore);
-*/
-
-  function playGame() {
-    {
-      const humanSelection = getHumanChoice();
-      const computerSelection = getComputerChoice();
-
-      console.log(playRound(humanSelection, computerSelection));
-      console.log("Usuario= ", humanScore);
-      console.log("Computadora= ", computerScore);
-    }
-
-    if (humanScore > computerScore) {
-      console.log("🎉 ¡Felicitaciones! Ganaste la partida.");
-    } else if (computerScore > humanScore) {
-      console.log("💻 La computadora ganó la partida. ¡Intentá nuevamente!");
-    } else {
-      console.log("🤝 La partida terminó en empate.");
-    }
-  }
-
-  playGame();
 });
